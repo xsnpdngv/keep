2f0a0f0ecf7ab4e4ae4a18ad2a7867be14d9377c
Tamás Dezső <dezso.t.tamas@gmail.com>
2017-05-28 21:32:07 +0200

    Update sdel.sh

--- sdel.sh
+++ sdel.sh
@@ -6,8 +6,7 @@ sdel="rm -f"
 command -v gshred >/dev/null 2>&1 && sdel="gshred -zvun 5"
 command -v  shred >/dev/null 2>&1 && sdel="shred -zvun 5"
 
-# read user input and perform delete
+# read user input and perform secure delete
 echo -n "Secure delete: $@? (y/N) "
 read is_sdel
-[ "$is_sdel" = "y" -o "$is_sdel" = "Y" ] && \
-$sdel "$@"
+[ "$is_sdel" = "y" -o "$is_sdel" = "Y" ] && $sdel "$@"
