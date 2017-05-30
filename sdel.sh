7781abfde459cb60a8526867560f3e17a704ff25
Tamas Dezso <dezso.t.tamas@gmail.com>
2017-05-30 18:16:45 +0200

    Update sdel.sh

--- sdel.sh
+++ sdel.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-# script to secure delete files
+#
+# sdel - secure delete files (shell script)
+#
+# usage: sdel.sh FILE...
+#
+# In case gshred or shred is available, files given as command line
+# arguments are going to be securely deletes, otherwise a simple
+# rm -f will take place.
 
-# select a command to delete with
+# check whether [g]shred is available to delete with
 sdel="rm -f"
 command -v gshred >/dev/null 2>&1 && sdel="gshred -zvun 5"
 command -v  shred >/dev/null 2>&1 && sdel="shred -zvun 5"
 
-# read user input and perform secure delete
-echo -n "Secure delete: $@? (y/N) "
-read is_sdel
-[ "$is_sdel" = "y" -o "$is_sdel" = "Y" ] && $sdel "$@"
+# read user input and perform delete
+echo -n "Delete ($sdel) file(s): $@? (y/N) "; read is_sdel
+[[ "${is_sdel:0:1}" =~ [yY] ]] && $sdel "$@"
